e486e8d6ad90c9fa4b5e1d0cb48705aad71754c8
javiinostroza <jminostroza@uc.cl>
2022-05-07 17:55:52 -0400

    Fix manifest and serviceworker settings

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,15 +1,33 @@
-const staticDevCoffee = "dev-coffee-site-v1"
+const staticDevDcc = "dev-dcc-site-v1"
 const assets = [
   "/",
   "/index.html",
-  "/styles.css",
-  "/main.js",
-  "/images/p1.jpg",
-]
+  "/css/style.css",
+  "/js/app.js",
+  "/images/Arenas.webp",
+  "/images/Baier.webp",
+  "/images/Eterovic.webp",
+  "/images/Herskovic.webp",
+  "/images/Lobel.webp",
+  "/images/Mery.webp",
+  "/images/Muñoz.webp",
+  "/images/Navon.webp",
+  "/images/Neyem.webp",
+  "/images/Nussbaum.webp",
+  "/images/Parra.webp",
+  "/images/Pichara.webp",
+  "/images/Reuter.webp",
+  "/images/Riveros.webp",
+  "/images/Ruz.webp",
+  "/images/Sandoval.webp",
+  "/images/Sepúlveda.webp",
+  "/images/Soto.webp",
+  "/images/Valdés.webp"
+];
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticDevDcc).then(cache => {
       cache.addAll(assets)
     })
   )
