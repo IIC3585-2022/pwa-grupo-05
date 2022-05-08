9553797f13b0dfe2802fcbb6c839c4cc8fd4c2b2
javiinostroza <jminostroza@uc.cl>
2022-05-07 22:17:25 -0400

    Clean code, remove unsubscription

--- js/app.js
+++ js/app.js
@@ -101,19 +101,16 @@ const teachers = [
 
 
 let pushButton = {"disabled": false};
-
 let isSubscribed = false;
 let swRegistration = null;
 
-function urlB64ToUint8Array(base64String) {
+const urlB64ToUint8Array = base64String => {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
   const base64 = (base64String + padding)
     .replace(/\-/g, '+')
     .replace(/_/g, '/');
-
   const rawData = window.atob(base64);
   const outputArray = new Uint8Array(rawData.length);
-
   for (let i = 0; i < rawData.length; ++i) {
     outputArray[i] = rawData.charCodeAt(i);
   }
@@ -131,7 +128,7 @@ const loadMyRequestCards = tabName => {
   requestContainer = document.getElementById(tabName)
   removeAllChildNodes(requestContainer)
   teachers.forEach(teacher => {
-    let teacherCard = document.getElementById(teacher.card_id).cloneNode(true);
+    teacherCard = document.getElementById(teacher.card_id).cloneNode(true);
     teacherCard.removeAttribute('id');
     teacherCard.id = teacher.card_id + '-request'
     let teacherButton = document.getElementById(teacher.card_id.split('-')[0])
@@ -170,48 +167,24 @@ const editButtonClass = (e, button) => {
   button.classList.add(class_);
 }
 
-
-function initialiseUI(teacherId, action) {
-
-  console.log("Initialize UI");
-
-  // pushButton.disabled = true;
-  // if (isSubscribed) {
-  //   unsubscribeUser();
-  // } else {
-  //   subscribeUser(teacherId);
-  // }
-    subscribeUser(teacherId, action);
-
-
-// Set the initial subscription value
-swRegistration.pushManager.getSubscription()
-.then(function(subscription) {
-  isSubscribed = !(subscription === null);
-  // updateSubscriptionOnServer(subscription);
-
-  if (isSubscribed) {
-    console.log('User IS subscribed.');
-  } else {
-    console.log('User is NOT subscribed.');
-  }
-
-  updateBtn();
-});
-
+const initialiseUI = (teacherId, action) => {
+  subscribeUser(teacherId, action);
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+    .then(subscription => {
+      isSubscribed = !(subscription === null);
+      updateBtn();
+    });
 };
 
-
 const handleClick = e => {
-  
   let data_ = e.firstChild.data == "Cancelar" ? "Enviar Solicitud" : "Cancelar"
   let button = document.getElementById(e.id); // Tomamos el botón a modificar
   pushButton = button;
   initialiseUI(e.id, e.firstChild.data)
   button.firstChild.data = data_; // Modificamos su texto
   if (data_ == "Enviar Solicitud") { 
-    // Se canceló la solicitud,
-    // Elimino tarjeta de Mis Solicitudes
+    // Se canceló la solicitud, elimino tarjeta de Mis Solicitudes
     var myRequests = document.getElementById("my-requests")
     var canceledTeacher = myRequests.querySelector("#"+ e.id + "-card-request")
     try {
@@ -222,83 +195,58 @@ const handleClick = e => {
 }
 
 if ('serviceWorker' in navigator && 'PushManager' in window) {
-  console.log('Service Worker and Push is supported');
-
   navigator.serviceWorker.register('serviceWorker.js')
-  .then(function(swReg) {
-    console.log('Service Worker is registered', swReg);
-
-    swRegistration = swReg;
-  })
-  .catch(function(error) {
-    console.error('Service Worker Error', error);
-  });
+    .then(swReg => {
+      swRegistration = swReg;
+    })
+    .catch(error => {
+      console.error('Service Worker Error', error);
+    });
 } else {
   console.warn('Push messaging is not supported');
 }
 
-
-function updateBtn() {
+const updateBtn = () => {
   if (Notification.permission === 'denied') {
     pushButton.disabled = true;
     updateSubscriptionOnServer(null, null, null);
     return;
   }
-
-
   pushButton.disabled = false;
 }
 
 navigator.serviceWorker.register('serviceWorker.js')
-.then(function(swReg) {
-  console.log('Service Worker is registered', swReg);
+.then(swReg => {
   swRegistration = swReg;
 })
 
-
-function subscribeUser(teacherId, action) {
+const subscribeUser = (teacherId, action) => {
   const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
   swRegistration.pushManager.subscribe({
     userVisibleOnly: true,
     applicationServerKey: applicationServerKey
   })
-  .then(function(subscription) {
-    console.log('User is subscribed:', subscription);
-
+  .then(subscription => {
     updateSubscriptionOnServer(subscription, teacherId, action);
-
     isSubscribed = true;
-
     updateBtn();
   })
-  .catch(function(err) {
+  .catch(err => {
     console.log('Failed to subscribe the user: ', err);
     updateBtn();
   });
 }
 
-
 const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
 
-
-
-function updateSubscriptionOnServer(subscription, teacherId, action) {
-  // TODO: Send subscription to application server
-
+const updateSubscriptionOnServer = (subscription, teacherId, action) => {
   let msge = action == "Enviar Solicitud" 
     ? "Se ha enviado su solicitud al profesor "
     : "Ha cancelado su solicitud al profesor "
-
   if (subscription) {
-
-    console.log(JSON.stringify(subscription));
-
     const subDetails = JSON.parse(JSON.stringify(subscription));
-    console.log(subDetails);
-    
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-
     const body = JSON.stringify({
       "msg": msge + teacherId,
       "endpoint": subDetails.endpoint,
@@ -313,32 +261,10 @@ function updateSubscriptionOnServer(subscription, teacherId, action) {
       body,
       redirect: 'follow'
     };
-    
     fetch("https://pwag5-api.herokuapp.com/subscribe", requestOptions)
       .then(response => response.text())
       .then(result => console.log(result))
       .catch(error => console.log('error', error));
-
   } 
-}
-
-function unsubscribeUser() {
-  swRegistration.pushManager.getSubscription()
-  .then(function(subscription) {
-    if (subscription) {
-      return subscription.unsubscribe();
-    }
-  })
-  .catch(function(error) {
-    console.log('Error unsubscribing', error);
-  })
-  .then(function() {
-    updateSubscriptionOnServer(null, null, null);
-
-    console.log('User is unsubscribed.');
-    isSubscribed = false;
-
-    updateBtn();
-  });
 }
 
