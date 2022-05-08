eca2cba7ad16f8b380e46d382345ad8fef62545e
javiinostroza <jminostroza@uc.cl>
2022-05-07 22:52:23 -0400

    Update icons

--- serviceWorker.js
+++ serviceWorker.js
@@ -48,7 +48,7 @@ self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-  const title = 'custom notification';
+  const title = 'DCC Profes';
   const options = {
     body: event.data.text(),
     icon: 'images/icon.png',
@@ -66,8 +66,5 @@ console.log('[Service Worker] Notification click Received.');
 
 event.notification.close();
 
-// event.waitUntil(
-//   clients.openWindow('https://developers.google.com/web/')
-// );
 });
 
